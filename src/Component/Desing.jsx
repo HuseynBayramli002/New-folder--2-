74da19085a73ea5c891330111ebea267d1183cf2
Huseynbayramli <huseynbayramli002@gmail.com>
2024-02-21 16:51:13 +0400

    img

--- src/Component/Desing.jsx
+++ src/Component/Desing.jsx
@@ -1,7 +1,7 @@
 
 const Desing = () => {
     return (
-        <div className="ml-10 flex gap-3">
+        <div className="ml-10 flex gap-3 relative">
             <div className="w-[4px] h-[314px] bg-white"></div>
             <div className="w-[2px] h-[407px] bg-white "></div>
             <div className="mt-10 ml-5">
@@ -23,6 +23,9 @@ const Desing = () => {
                 </div>
                 <p className="mt-[10px] mb-[30px] text-sm text-white">Suni intelekt sistemi</p>
                 <p className=" text-white w-60 text-sm ">Bu formu doldurduqdan sonra öz yaşıdlarınız arasında ən yaxşı hansı faizlikdə olduğunuzu müəyyən edə biləcəksiniz.</p>
+            <div className=" absolute w-60 bottom-24 left-60">
+                <img src="/public/komp.svg" alt="" />
+            </div>
             </div>
         </div>
     )
